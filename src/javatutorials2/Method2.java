f175c300cf3e6cdb4324ca157f4fc22653c34ae5
fbwkzl333 <fbwkzl111@naver.com>
2016-10-01 01:14:27 +0900

    메소드 void 설명, return

---
+++ src/javatutorials2/Method2.java
@@ -0,0 +1,23 @@
+package javatutorials2;
+
+public class Method2 {
+	public static String numbering(int init, int limit) {
+		//		  Data타입  메소드 이름
+		int i = init;
+		String output = "";
+		while (i < limit){
+			output += i;
+			i++;
+		}
+		return output;
+	}
+	
+	
+	public static void main(String[] args) {
+		//		 void 받는다 = 메소드들의 리턴한 값을 담는다.
+		String result = numbering(1, 5);
+		// numbering메소드의 return 값을 result로 받음
+		System.out.println(result);
+	}
+
+}
